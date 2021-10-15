fd3c143557ccb427e794bd884151fb763f841bbd
luoxiao <1807657825@qq.com>
2021-10-15 18:45:15 +0800

    update

--- src/ee-header.ts
+++ src/ee-header.ts
@@ -1,86 +1,92 @@
 // import { store } from "@/index";
-import {  css, html, LitElement } from "lit";
-import {customElement, property} from 'lit/decorators.js';
+import { css, html, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 
-@customElement('ee-header')
-export class Header extends LitElement  {
-    // static mtagName = "ee-header"
-    // constructor() {
-    //     super();
-    //     const shadow = this.attachShadow({ mode: 'open' });
+@customElement("ee-header")
+export class Header extends LitElement {
+  // static mtagName = "ee-header"
+  // constructor() {
+  //     super();
+  //     const shadow = this.attachShadow({ mode: 'open' });
 
-    // }
-    static styles = css`
-    ul{
-        list-style-type:none;
+  // }
+  static styles = css`
+    ul {
+      list-style-type: none;
     }
-    li{
-        display:inline-block;
-        color: #ffffff;
+    li {
+      display: inline-block;
+      color: #ffffff;
     }
-    `
-    // get name() {
-    //     return this.getAttribute('name');
-    // }
+  `;
+  // get name() {
+  //     return this.getAttribute('name');
+  // }
 
-    // get age() {
-    //     return this.getAttribute('age');
-    // }
+  // get age() {
+  //     return this.getAttribute('age');
+  // }
 
-    render() {
-        return html`
-        <div>
+  @state()
+  protected username = 'luoxiao';
+
+  render() {
+    return html`
+      <div>
         <ul>
-            <li>
-            <a>
-            notification
-            </a>
-            </li>
+          <li>
+            <a> notification </a>
+          </li>
+          <li (click)="change()">
+            <a> ${this.username} </a>
+          </li>
         </ul>
-        </div>
+      </div>
     `;
-    }
+  }
 
-    // afterViewChecked() {
-    // }
-    // unsubscribe
-    // html
-    // connectedCallback() {
-    //     // this.shadowRoot.querySelector('input').addEventListener('click',(e)=>{
-    //     //     e.preventDefault();
-    //     // });
-    //     // bindEventsMethods(this);
-    //     // this.unsubscribe = store.subscribe(() => {
-    //     //     let html = this.render(store.getState());
-    //     //     if (this.html !== html) {
-    //     //         this.html = html;
-    //     //         this.shadowRoot.innerHTML = html;
-    //     //     }
-    //     //     this.afterViewChecked();
-    //     // });
-    //     // let html = this.render(store.getState());
-    //     //     if (this.html !== html) {
-    //     //         this.html = html;
-    //     //         this.shadowRoot.innerHTML = html;
-    //     //     }
-    //     //     this.afterViewChecked();
-    // }
-    // disconnectedCallback() {
-    //     // this.unsubscribe();
-    // }
+  change(){
+      this.username += 1;
+  }
 
-    // static get observedAttributes() { return ['name', 'age']; }
+  // afterViewChecked() {
+  // }
+  // unsubscribe
+  // html
+  // connectedCallback() {
+  //     // this.shadowRoot.querySelector('input').addEventListener('click',(e)=>{
+  //     //     e.preventDefault();
+  //     // });
+  //     // bindEventsMethods(this);
+  //     // this.unsubscribe = store.subscribe(() => {
+  //     //     let html = this.render(store.getState());
+  //     //     if (this.html !== html) {
+  //     //         this.html = html;
+  //     //         this.shadowRoot.innerHTML = html;
+  //     //     }
+  //     //     this.afterViewChecked();
+  //     // });
+  //     // let html = this.render(store.getState());
+  //     //     if (this.html !== html) {
+  //     //         this.html = html;
+  //     //         this.shadowRoot.innerHTML = html;
+  //     //     }
+  //     //     this.afterViewChecked();
+  // }
+  // disconnectedCallback() {
+  //     // this.unsubscribe();
+  // }
 
+  // static get observedAttributes() { return ['name', 'age']; }
 
-    // changeName($event, name, age) {
-    //     this.setAttribute('name', name + 1);
-    // }
-    // changeAge($event, name, age) {
-    //     this.setAttribute('age', age + 1);
-    // }
+  // changeName($event, name, age) {
+  //     this.setAttribute('name', name + 1);
+  // }
+  // changeAge($event, name, age) {
+  //     this.setAttribute('age', age + 1);
+  // }
 
-    // attributeChangedCallback() {
-       
+  // attributeChangedCallback() {
 
-    // }
+  // }
 }
